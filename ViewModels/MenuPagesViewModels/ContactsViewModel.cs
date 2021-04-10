8a16c514c3824b99868641d203db6ef9e32be300
Дмитрий Шалунов <schalunov@bk.ru>
2021-04-10 20:28:37 +0300

    add contacts page

---
+++ ViewModels/MenuPagesViewModels/ContactsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+namespace ComputerPeripheralsShop.ViewModels.MenuPagesViewModels
+{
+    internal class ContactsViewModel
+    {
+        public ICommand ToForum { get; }
+
+        public ContactsViewModel()
+        {
+            ToForum = new CommandViewModel(executeToForum);
+        }
+
+        private void executeToForum()
+        {
+            OpenUrl("https://4pda.ru/forum/index.php?showtopic=907116");
+        }
+
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
