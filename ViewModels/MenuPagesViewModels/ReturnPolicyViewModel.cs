5717ce1016f3f3365d43318f97092ca7253da307
Дмитрий Шалунов <schalunov@bk.ru>
2021-04-13 01:04:56 +0300

    add PrivatePolicy page

---
+++ ViewModels/MenuPagesViewModels/ReturnPolicyViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ComputerPeripheralsShop.ViewModels.MenuPagesViewModels
+{
+    internal class ReturnPolicyViewModel
+    {
+        public ICommand ToContacts { get; }
+
+        public ReturnPolicyViewModel()
+        {
+            ToContacts = new CommandViewModel(executeToContacts);
+        }
+
+        private void executeToContacts()
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    /*(window as MainWindow).MainWindowFrame.Navigate(new Uri(SubName, UriKind.RelativeOrAbsolute));*/
+                    (window as MainWindow).MainWindowFrame.Navigate(new Uri(string.Format("{0}{1}{2}", "Views/Pages/MenuPages/", "Contacts", ".xaml"), UriKind.RelativeOrAbsolute));
+                }
+            }
+        }
+    }
+}
